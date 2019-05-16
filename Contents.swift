39e73779d26fb93ce7512ee55866ab38f21a9556
Eashir <eashir.arafat@gmail.com>
2019-05-16 01:30:35 -0400

    Unique pair, slices

--- Contents.swift
+++ Contents.swift
@@ -1,9 +1,34 @@
 import UIKit
 
-var gotCharArray = "Aegar Targaryen, Rob Stark, Arya Stark"
+var gotCharArray = [ "Aegon Targaryen", "Rob Stark", "Arya Stark"]
 
 var gotCharArrayIterator = gotCharArray.makeIterator()
 
 while let gotChar = gotCharArrayIterator.next() {
 	print(gotChar)
 }
+
+for value in repeatElement(24, count: 5) {
+	print(value)
+}
+
+
+let letters = ["E", "V", "A"]
+//All unique pairs
+
+for (index, firstLetter) in letters.enumerated() {
+	for secondLetter in letters.dropFirst(index + 1) {
+		print(firstLetter, secondLetter)
+	}
+}
+
+
+//MARK: Slices
+
+let numArray = Array(0...99)
+let all = numArray[...]
+
+let lower = numArray[..<50]
+let upper = numArray[50...]
+let some = numArray[20...50]
+let more = numArray[10..<11]
